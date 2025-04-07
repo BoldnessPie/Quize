e7acfe2f07268efb828494ed908a407c4305f78e
Alex Haun <boldnesspie@gmail.com>
2025-04-07 23:08:30 +0300

    add data to quize-data

--- components/quize/quize-data.js
+++ components/quize/quize-data.js
@@ -34,4 +34,40 @@ export default [
       },
     },
   },
+  {
+    question:
+      "В каком году появились знаменитые строки: мерс папин, жопа мамина? ",
+    answers: {
+      Васич: {
+        pic: "../../img-vasich.png",
+        points: 0,
+        message: "Было, но свидетелей нет",
+      },
+      Севан: {
+        pic: "../../img-sevan.png",
+        points: 0,
+        message: "Тайланд, о чем тут говорить брат",
+      },
+      Машкин: {
+        pic: "../../img-mashkin.png",
+        points: 0,
+        message: "Подозрительно много видео с трансами на телефоне",
+      },
+      Андрюха: {
+        pic: "../../img-andruha.png",
+        points: 2,
+        message: "Чист, как слеза ребенка",
+      },
+      Крошка: {
+        pic: "../../img-kroshka.png",
+        points: 0,
+        message: "50/50, слишком подозрителньая личность",
+      },
+      Гаун: {
+        pic: "../../img-kroshka.png",
+        points: 0,
+        message: ")))0 сами знаете ребята",
+      },
+    },
+  },
 ];
