c9d6f40761937da59c28039dfefa6d71db93b6a1
Alex Haun <boldnesspie@gmail.com>
2025-04-07 23:04:47 +0300

    Initial commit

---
+++ components/quize/quize-data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    question: "Кто из этих людей никогда не спал с трансом?",
+    answers: {
+      Васич: {
+        pic: "../../img-vasich.png",
+        points: 0,
+        message: "Было, но свидетелей нет",
+      },
+      Севан: {
+        pic: "../../img-sevan.png",
+        points: 0,
+        message: "Тайланд, о чем тут говорить брат",
+      },
+      Машкин: {
+        pic: "../../img-mashkin.png",
+        points: 0,
+        message: "Подозрительно много видео с трансами на телефоне",
+      },
+      Андрюха: {
+        pic: "../../img-andruha.png",
+        points: 2,
+        message: "Чист, как слеза ребенка",
+      },
+      Крошка: {
+        pic: "../../img-kroshka.png",
+        points: 0,
+        message: "50/50, слишком подозрителньая личность",
+      },
+      Гаун: {
+        pic: "../../img-kroshka.png",
+        points: 0,
+        message: ")))0 сами знаете ребята",
+      },
+    },
+  },
+];
